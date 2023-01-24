13cc02bc49e15a1c3250949dbefbe5bf8ee03cdf
swarna <kumariswarna083@gmail.com>
2023-01-24 22:47:23 +0530

    adding index

---
+++ Youtubedom/index.js
@@ -0,0 +1,8 @@
+console.log(document.getElementById('header-title'));
+let heading=document.getElementById('header-title');
+heading.innerHTML="<h1>Hello</h1>";
+heading.style.borderBottom="solid 3px Black";
+console.log(heading);
+console.log(document.all[14].textContent="ADD ITEMS");
+console.log(document.all[14].innerHTML="<h1>ADD ITEMS</h1>");
+console.log(document.all[14].style.color="green");
