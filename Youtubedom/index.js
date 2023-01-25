aa1a659488ad172786a6e31848c6de644d5475e4
swarna <kumariswarna083@gmail.com>
2023-01-25 11:07:55 +0530

    add index

--- Youtubedom/index.js
+++ Youtubedom/index.js
@@ -6,3 +6,9 @@ console.log(heading);
 console.log(document.all[14].textContent="ADD ITEMS");
 console.log(document.all[14].innerHTML="<h1>ADD ITEMS</h1>");
 console.log(document.all[14].style.color="green");
+let items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor="green";
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold";
+}
+console.log(items);
